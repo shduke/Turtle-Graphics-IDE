f19c968881bd34a865702d7c4d2e72704eda5883
Naijiao <nz21@duke.edu>
2016-10-21 01:34:59 -0400

    Renamed properties

--- src/parser/TestParser.java
+++ src/parser/TestParser.java
@@ -4,6 +4,7 @@ public class TestParser {
 
 	public static void main(String[]args){
 		InputParser test = new InputParser(); 
+		System.out.println(test.getSymbol(":what"));
 		System.out.println(test.getSymbol("what"));
 		System.out.println(test.getSymbol("+"));
 		System.out.println(test.getSymbol("2"));
