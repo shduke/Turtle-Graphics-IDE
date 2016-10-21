a09913d18cae64f354b4f1ed4155ad56f89166ec
Sean Hudson <srh50@duke.edu>
2016-10-21 05:59:28 -0400

    added Coordinate class and edited Cursor

---
+++ src/command/Forward.java
@@ -0,0 +1,17 @@
+package command;
+
+import cursor.Cursor;
+
+public class Forward extends CursorCommand {
+
+    Forward (Cursor cursor) {
+        super(cursor);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void execute() {
+        
+    }
+    
+}
