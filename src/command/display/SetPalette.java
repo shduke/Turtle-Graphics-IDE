20cdc452048599f279901128966bd605f9fd1a98
Sean Hudson <srh50@duke.edu>
2016-11-02 08:37:19 -0400

    BugFix infinite loop

---
+++ src/command/display/SetPalette.java
@@ -0,0 +1,22 @@
+package command.display;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+import behavior.unary.DoubleUnaryBehavior;
+import behavior.unary.MovementBehavior;
+import command.AbstractCommand;
+import command.utility.Constant;
+import command.utility.Variable;
+import cursor.Coordinate;
+import cursor.ICursor;
+
+public class SetPalette extends AbstractCommand {
+    private static final int MY_NUMBER_OF_COMMAND_PARAMETERS = 4;
+    
+    public SetPalette (ICursor cursor, AbstractCommand... arguments) {
+        super(new DoubleUnaryBehavior(cursor::setBackground, arguments[0], new Constant(-1.0)));
+
+    }
+    
+}
