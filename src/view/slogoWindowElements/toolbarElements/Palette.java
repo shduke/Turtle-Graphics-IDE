7969795fe8304f8b7ee3c2552f217229dafcb8e3
Noel Moon <nm142@duke.edu>
2016-11-02 01:41:38 -0400

    pallet

--- src/view/slogoWindowElements/toolbarElements/Palette.java
+++ src/view/slogoWindowElements/toolbarElements/Palette.java
@@ -6,7 +6,13 @@ package view.slogoWindowElements.toolbarElements;
 import java.awt.Dimension;
 import java.util.List;
 
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
 
 /**
  * @author Noel Moon (nm142)
@@ -14,20 +20,23 @@ import javafx.scene.layout.HBox;
  */
 public abstract class Palette extends Property {
 	
-	public static final Dimension DEFAULT_SIZE = new Dimension(800, 200);
-
-	private List<?> myPalette;
+	public static final Dimension DEFAULT_SIZE = new Dimension(500, 120);
 	
-	public Palette(HBox palette) {
+	public Palette() {
 		super(DEFAULT_SIZE.getWidth(), DEFAULT_SIZE.getHeight());
-		setRoot(palette);
 	}
 	
-	public List<?> getPalette() {
-		return myPalette;
-	}
-	
-	protected void makePalette(List<?> list) {
-		
+	protected HBox makePalette(List<Node> list) {
+		HBox hb = new HBox();
+		for (int i=0; i<5; i++) {
+			VBox vb = new VBox();
+			vb.getChildren().add(list.get(i));
+			Label label = new Label(Integer.toString(i));
+			label.setPrefWidth(100);
+			label.setAlignment(Pos.TOP_CENTER);
+			vb.getChildren().add(label);
+			hb.getChildren().add(vb);
+		}
+		return hb;
 	}
 }
